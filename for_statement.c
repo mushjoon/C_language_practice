cdad98bf09f8f54947bfa0b754c775ef923f3c8d
Jack Kim <kimhyjun@umich.edu>
2022-01-20 17:59:03 +0900

    Solved Exercise 1-5
    Solved Exercise 1-5

--- for_statement.c
+++ for_statement.c
@@ -4,8 +4,14 @@
 int main(){
     int fahr;
 
-    for(fahr = 0; fahr <= 300; fahr = fahr + 20)
+    // for(fahr = 0; fahr <= 300; fahr = fahr + 20){
+    //     printf("%3d \t%6.2f\n", fahr, (5.0/9.0)*(fahr-32));
+    // }
+    
+    // Exercise 1-5: print the table in reverse order, from 300 degrees to 0
+    for(fahr = 300; fahr >= 0; fahr = fahr - 20) 
         printf("%3d \t%6.2f\n", fahr, (5.0/9.0)*(fahr-32));
     
+
     return 0;
 }
