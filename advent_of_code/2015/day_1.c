018f9c6c67d620db830afb8421f6e00d699d75d9
Jack Kim <kimhyjun@umich.edu>
2022-01-27 14:43:11 +0900

    Advent of Code 2015 Day1 Part1 solved!
    Advent of Code 2015 Day1 Part1 solved! Yay~

--- advent_of_code/2015/day_1.c
+++ advent_of_code/2015/day_1.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 
     FILE *pMyfile = fopen("day_1_input.txt", "r");
+
+    char buffer[2000000] = {0};
+    int steps = 0;
+
+    if (pMyfile == NULL)
+    {
+        printf("Unable to open file!\n");
+    }
+    else
+    {
+        while (fgets(buffer, 2000000, pMyfile) != NULL)
+        {
+            for (int i = 0; i < sizeof(buffer) / sizeof(buffer[0]); i++)
+            {
+                if (buffer[i] == '(')
+                {
+                    steps++;
+                }
+                else if (buffer[i] == ')')
+                {
+                    steps--;
+                }
+            }
+        }
+        printf("Santa is currently at %d floor\n", steps);
+    }
 
     return 0;
 }
---
+++ advent_of_code/2015/tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+for (int i = 0; i < sizeof(buffer) / sizeof(buffer[0]); i++)
+            // {
+                
+            //     if (buffer[i] == '(')
+            //     {
+            //         steps++;
+            //     }
+            //     else if(buffer[i] == ')')
+            //     {
+            //         steps--;
+            //     }
+                
+            // }
