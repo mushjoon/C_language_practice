6721a65de4adfda9e24e6ba6ffedace5586e9525
Jack Kim <kimhyjun@umich.edu>
2022-02-24 18:00:30 +0900

    Started File Input and Output chapter
    Started File Input and Output chapter

---
+++ C_Programming_Udemy/Section_14/accessing_files.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    /* 
+    The fopen() function is the start of accessing files
+
+    FILE *fopen(const char *restrict name, const char *restrict mode);
+
+    The 1st arg = pointer to a string that is the name of the external file you wanna process
+    */
+
+    return 0;
+}
