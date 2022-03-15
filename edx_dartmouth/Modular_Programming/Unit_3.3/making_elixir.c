777b49740ba23464db471b0327d18f34928a2a2f
Jack Kim <kimhyjun@umich.edu>
2022-03-15 23:41:44 +0900

    pointer challenge 1 finished
    pointer challenge 1 finished

---
+++ edx_dartmouth/Modular_Programming/Unit_3.3/making_elixir.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age;
+
+    int *ageptr = NULL;
+
+    scanf("%d", &age);
+
+    ageptr = &age;
+
+    printf("The secret address is... ");
+
+    printf("%p\n", ageptr);
+
+    printf("Now take three drops of the magic elixir. \n");
+
+    *ageptr -= 5;
+
+    printf("Did the elixir work? You are %d years old!", age);
+
+    return 0;
+}
